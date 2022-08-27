20786fb6ff238349ad98b8f03cbef007112e7ff2
MoonLGH <MoonLMC1212@gmail.com>
2022-08-27 13:15:11 +0000

    tailwind fixed, now im gonna punch my self on sidebar

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,9 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/*.html', './node_modules/flowbite/**/*.js', '*.ejs'],
+  content: ['./src/app/views/*.ejs', './src/app/views/**/*.ejs'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'keqing-main': '#ba95c8',
+        'keqing-secondary': '#55336b',
+        'keqing-secondary-jumbotron': '#916ba8',
+        'keqing-tresier': '#d1b1c7',
+        'keqing-tresier2': '#aa89bf',
+        'keqing-tresier3': '#f4f2f5',
+      },
+    },
   },
   plugins: [require('daisyui')],
 
-}
+};
